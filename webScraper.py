451bbca0b412198bcee8444348c2c84d68bc3e8e
Damond <damondfg@gmail.com>
2022-07-09 22:04:13 -0700

    added account info

--- webScraper.py
+++ webScraper.py
@@ -3,8 +3,6 @@ from selenium import webdriver
 from pymongo import MongoClient
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver import chrome
 
 
 # CLuster Database
@@ -19,9 +17,9 @@ driver.get("https://www.twitter.com/login")
 time.sleep(1)
 
 #account info
-email = "<email>"
-password = "<password>"
-username = "<username>" #For security check
+email = "daocodedayproject@gmail.com"
+password = "Damond900"
+username = "codedayproject" #For security check
 
 #login xpaths
 email_xpath = "//input[@autocomplete=\"username\"]"
@@ -32,18 +30,18 @@ security_xpath = "//input[@autocomplete=\"on\"]"
 username_input = driver.find_element(By.XPATH, email_xpath)
 username_input.send_keys(email)
 username_input.send_keys(Keys.RETURN)
-time.sleep(0.5)
+time.sleep(1)
 
 #security check
 
 security_input = driver.find_element(By.XPATH, security_xpath)
 security_input.send_keys(username)
 security_input.send_keys(Keys.RETURN)
-time.sleep(0.5)
+time.sleep(1)
 
 
 #filling in password
 password_input = driver.find_element(By.XPATH, password_xpath)
 password_input.send_keys(password)
 password_input.send_keys(Keys.RETURN)
-time.sleep(0.5)
+time.sleep(1)

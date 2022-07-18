f6094b25dc01e2ef24534021c9e8afe0ba1f17a0
Damond <damondfg@gmail.com>
2022-07-18 00:08:59 -0700

    update

---
+++ web/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <p>{{ name }}</p>
+    <button @click="changeName('adfsdf')">change name</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    
+  },
+  data() {
+    return {
+      name: 'link'
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name
+    }
+  }
+});
+</script>
+
+<style>
+
+</style>

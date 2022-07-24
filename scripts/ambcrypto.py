c52ee6e5954efa9827d604e788677b30b5d372cb
Damond <damondfg@gmail.com>
2022-07-24 16:56:06 -0700

    update

--- scripts/ambcrypto.py
+++ scripts/ambcrypto.py
@@ -7,52 +7,63 @@ from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import requests
 
-# CLuster Database
-cluster = MongoClient('mongodb+srv://damond:O76IfcRsXU1YxoPU@cluster0.qxhzvno.mongodb.net/?retryWrites=true&w=majority')
-database = cluster['data']
-collection = database['google_news']
 
-#Setting chrome driver
-PATH = 'C:\Program Files (x86)\chromedriver.exe'
-driver = webdriver.Chrome(PATH)
 
-driver.get('https://ambcrypto.com/')
+class ambcrypto:
+    def __init__(self, search_term):
+        #Setting chrome driver
+        PATH = 'C:\Program Files (x86)\chromedriver.exe'
+        driver = webdriver.Chrome(PATH)
 
-time.sleep(4)
+        driver.get('https://ambcrypto.com/')
 
-search_term = 'aave'
+        time.sleep(4)
 
-ActionChains(driver)
-try:
-    search_button = driver.find_element(By.XPATH, './/span[@class="mvp-nav-search-but fa fa-search fa-2 mvp-search-click"]')
-    search_button.click()                                                                                                                                                                                                                                                                                                       
-except:
-    pass
 
-time.sleep(1)
 
-search = driver.find_element(By.XPATH, './/div[@id="mvp-search-box"]//input')
-search.send_keys(search_term)
-time.sleep(0.5)
-try:
-    search.send_keys(Keys.RETURN)
-except:
-    pass
+    def navigate(self, search_term, driver):
+        ActionChains(driver)
+        try:
+            search_button = driver.find_element(By.XPATH, './/span[@class="mvp-nav-search-but fa fa-search fa-2 mvp-search-click"]')
+            search_button.click()                                                                                                                                                                                                                                                                                                       
+        except:
+            pass
 
-time.sleep(2)
+        time.sleep(1)
 
-articles = driver.find_elements(By.XPATH, './/li[@class="mvp-blog-story-wrap left relative infinite-post"]')
-print(len(articles))
+        search = driver.find_element(By.XPATH, './/div[@id="mvp-search-box"]//input')
+        search.send_keys(search_term)
+        time.sleep(0.5)
+        try:
+            search.send_keys(Keys.RETURN)
+        except:
+            pass
 
-article_link = articles[1].find_element(By.XPATH, './/a').get_attribute('href')
+        time.sleep(2)
 
-print(article_link)
 
-r = requests.get(article_link)
-soup = BeautifulSoup(r.text, 'html.parser')
-paragraphs = soup.find_all('p')
 
-print(paragraphs)
+    def get_data(self, driver):
+
+        articles = driver.find_elements(By.XPATH, './/li[@class="mvp-blog-story-wrap left relative infinite-post"]')
+        print(len(articles))
+
+        article_link = articles[1].find_element(By.XPATH, './/a').get_attribute('href')
+
+        r = requests.get(article_link)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        paragraphs = soup.find_all('p')
+
+        print(paragraphs)
+
+
+    def writeData(self, data):
+        # CLuster Database
+        cluster = MongoClient('mongodb+srv://damond:O76IfcRsXU1YxoPU@cluster0.qxhzvno.mongodb.net/?retryWrites=true&w=majority')
+        database = cluster['data']
+        collection = database['ambcrypto']
+
+        collection.insert_one(data)
 
 
 
--- scripts/newsScraper.py
+++ scripts/newsScraper.py
@@ -4,97 +4,79 @@ from pymongo import MongoClient
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-import requests
-
-
-# CLuster Database
-cluster = MongoClient('mongodb+srv://damond:O76IfcRsXU1YxoPU@cluster0.qxhzvno.mongodb.net/?retryWrites=true&w=majority')
-database = cluster['data']
-collection = database['google_news']
-
-#Setting chrome driver
-PATH = 'C:\Program Files (x86)\chromedriver.exe'
-driver = webdriver.Chrome(PATH)
-
-driver.get('https://www.google.com/')
-
-time.sleep(2)
-
-search_term = 'aave coin yahoo'
 
 
 
-# clicks next in google search
-def click_next():
-    ActionChains(driver)
-    next_button_click = driver.find_element(By.XPATH, '//*[@id="pnnext"]/span[2]')
-    next_button_click.click()
+class googleNews:
+    def __init__(self, search_term):
+        PATH = 'C:\Program Files (x86)\chromedriver.exe'
+        driver = webdriver.Chrome(PATH)
+
+        driver.get('https://www.google.com/')
+        time.sleep(2)
+
+        article_elements = self.navigate(search_term, driver)
+        time.sleep(2)
+
+        for article_element in article_elements:
+            self.writeData(self.get_data(article_element))
+        
+        driver.quit()
+
+
+    # clicks next in google search
+    def click_next(self, driver):
+        ActionChains(driver)
+        next_button_click = driver.find_element(By.XPATH, '//*[@id="pnnext"]/span[2]')
+        next_button_click.click()
 
 
 
-#searches the term and returns list of article elements
-def navigate(search_term):
-    search = driver.find_element(By.XPATH, './/div/input[@class="gLFyf gsfi"]')
-    search.send_keys(search_term)
-    search.send_keys(Keys.RETURN)
+    #searches the term and returns list of article elements
+    def navigate(self, search_term, driver):
+        search = driver.find_element(By.XPATH, './/div/input[@class="gLFyf gsfi"]')
+        search.send_keys(search_term)
+        search.send_keys(Keys.RETURN)
 
-    time.sleep(2)
-    news_button_click = driver.find_element(By.XPATH, './/div[@class="MUFPAc"]//a').get_attribute('href')
-    driver.get(news_button_click)
+        time.sleep(2)
+        news_button_click = driver.find_element(By.XPATH, './/div[@class="MUFPAc"]//a').get_attribute('href')
+        driver.get(news_button_click)
 
-    time.sleep(2)
+        time.sleep(2)
 
-    articles = driver.find_elements(By.XPATH, '//*[@id="rso"]/div')
+        articles_elements = driver.find_elements(By.XPATH, '//*[@id="rso"]/div')
 
-    return articles
-
-
-# takes in article element and returns title, description, link
-def get_data(article):
-    article_title = article.find_element(By.XPATH, './/div[@class="mCBkyc y355M ynAwRc MBeuO nDgy9d"]').text
-    article_description = article.find_element(By.XPATH, './/div[@class="GI74Re nDgy9d"]').text
-    link = article.find_element(By.XPATH, './/a[@class="WlydOe"]').get_attribute('href')
-    
-
-    print(article_title)
-    print(article_description)
-    print(link)
-
-    data = {
-        'article title': article_title,
-        'article description': article_description,
-        'link': link
-    }
-    return data
-
-coin = '$MANA'
-
-article = get_data(navigate(coin + ' coin yahoo')[0])
-
-url = article['link']
+        return articles_elements
 
 
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
-paragraphs = soup.find_all('p')
 
-print(paragraphs)
+    # takes in article element and returns title, description, link
+    def get_data(self, article):
+        article_title = article.find_element(By.XPATH, './/div[@class="mCBkyc y355M ynAwRc MBeuO nDgy9d"]').text
+        article_description = article.find_element(By.XPATH, './/div[@class="GI74Re nDgy9d"]').text
+        link = article.find_element(By.XPATH, './/a[@class="WlydOe"]').get_attribute('href')
+        
 
-p2 = [paragraph.text for paragraph in paragraphs]
+        print(article_title)
+        print(article_description)
+        print(link)
 
-print(p2)
-
-p3 = []
-
-for paragraph in p2:
-    if ('$MANA' in paragraph):
-        p3.append(paragraph)
-
-print(p3)
+        data = {
+            'article title': article_title,
+            'article description': article_description,
+            'link': link
+        }
+        return data
 
 
-time.sleep(11111)
 
-driver.quit()
+    def writeData(self, data):
+        # CLuster Database
+        cluster = MongoClient('mongodb+srv://damond:O76IfcRsXU1YxoPU@cluster0.qxhzvno.mongodb.net/?retryWrites=true&w=majority')
+        database = cluster['data']
+        collection = database['google_news']
+
+        collection.insert_one(data)
+
+
 

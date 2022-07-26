788f8fcad6609542aa2628ac8d432d663c37aaf8
Damond <damondfg@gmail.com>
2022-07-26 02:58:58 -0700

    final update

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from textblob import TextBlob
 import statistics
 
+
 def get_data(coin_symbol):
     cluster = MongoClient('mongodb+srv://damond:O76IfcRsXU1YxoPU@cluster0.qxhzvno.mongodb.net/?retryWrites=true&w=majority')
     database = cluster['data']
@@ -16,22 +17,21 @@ def get_data(coin_symbol):
 
 
 
-def analysis(results):
+def analyze(results):
     scores = []
     for data in results:
         title_score = TextBlob(data['article title']).sentiment
         description_score = TextBlob(data['article description']).sentiment
         scores.append(title_score[0])
         scores.append(description_score[0])
-
+    
     filtered = []
     for score in scores:
-        if score != 0:
+        if score > 0.25 or score < -0.25:
             filtered.append(score)
-    
+
     score = statistics.mean(filtered)
+    stringScore = str(score)
 
-    return score
+    return stringScore
         
-
-analysis(get_data('aave'))

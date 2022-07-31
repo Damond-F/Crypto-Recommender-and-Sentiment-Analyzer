66e9a73fc524a36b911d8cb5ee1110352fbe4e10
Damond-F <96760391+Damond-F@users.noreply.github.com>
2022-07-30 21:08:35 -0700

    Update analysis.py

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -4,7 +4,7 @@ import statistics
 
 
 def get_data(coin_symbol):
-    cluster = MongoClient('mongodb+srv://damond:O76IfcRsXU1YxoPU@cluster0.qxhzvno.mongodb.net/?retryWrites=true&w=majority')
+    cluster = MongoClient('mongodb+srv://user:pass@cluster0.qxhzvno.mongodb.net/?retryWrites=true&w=majority')
     database = cluster['data']
     google_collection = database['google_news']
 
